9b5954573a2619710d5ea50bdfbe2be6d940faf2
Mary Serova <nonscriverescript@gmail.com>
2025-04-21 02:21:35 +0300

    test mobile 2

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -3,85 +3,88 @@ import Header from "../components/general/Header.jsx";
 import Footer from "../components/general/Footer.jsx";
 import mah from "assets/mah.png";
 import mosc from "assets/mosc.png";
+import mah2 from "assets/mahM.png";
+import mosc2 from "assets/mosM.png";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Map({bitrixData}) {
+
+    const isDesktop = useMediaQuery ({minWidth: 768})
 
     return (
         <>
             <div id="main-page-wrapper">
                 <Header />
                 <section >
-                    <div className="map-block row">
-                        <h1 className=" text-center"> Контакты </h1>
+                    <div className="map-block ">
+                        <h1 className=" text-center mb-md-5 mb-4 mt-0"> Контакты </h1>
+
+                        <div className="row">
 
                             <div className="map-block__image col-12 col-md-4">
-                                <img src= alt="" />
+                                <img className="img-fluid" src={isDesktop ? mah : mah2} alt="Махачкала" />
                             </div>
-                            <div className="map-block__block col-12 col-md-8">
+                            <div className="map-block__block col-12 col-md-8 row">
 
-                            </div>
+                                <h2 className="col-12"> Махачкала </h2>
+                                <div className="col-6">
+                                    <p className="light">  улица Аскерханова, 1В </p>
+                                    <div>
+                                        <span className="my-1"> {bitrixData[0].NUM1 }</span>
+                                        <span >{bitrixData[0].NUM2 }</span>
+                                        <span >{bitrixData[0].MAIL }</span>
+                                    </div>
 
-                                <div className="map-block__map__contacts">
-                                    <div className="contacts__makhachkala">
-                                        <h3> Город Махачкала </h3>
-                                        <div className="contacts__makhachkala__numbers">
-                                            <p> улица Аскерханова, 1В </p>
-                                            <span className="my-1"> {bitrixData[0].NUM1 }</span>
-                                            <span >{bitrixData[0].NUM2 }</span>
-                                            <span >{bitrixData[0].MAIL }</span>
-                                        </div>
+                                    <div>
+                                        <p> Режим работы: </p>
+                                        <span> <span className="light"> будние - </span> {bitrixData[0].DAYS }</span>
+                                        <span>  <span className="light">выходные - </span> {bitrixData[0].ENDS }</span>
+                                        <span> <span className="light">последнее воскресенье месяца -   </span>  {bitrixData[0].SAT }    </span>
+                                    </div>
+                                </div>
 
-                                        <div className="contacts__makhachkala__numbers">
-                                            <p> Режим работы: </p>
-                                            <span> <span className="light"> будние - </span> {bitrixData[0].DAYS }</span>
-                                            <span>  <span className="light">выходные - </span> {bitrixData[0].ENDS }</span>
-                                            <span> <span className="light">последнее воскресенье месяца -   </span>  {bitrixData[0].SAT }    </span>
-                                        </div>
+                                <div className="col-6">
+                                    <p className="light"> улица Хурукринская, 11 </p>
+
+                                    <div>
+                                        <span className="my-1"> {bitrixData[1].NUM1 }</span>
+                                        <span >{bitrixData[1].NUM2 }</span>
+                                        <span >{bitrixData[1].MAIL }</span>
+                                    </div>
+
+                                    <div>
+
+                                        <p> Режим работы: </p>
+                                        <span> <span className="light"> будние - </span> {bitrixData[1].DAYS }</span>
+                                        <span>  <span className="light">выходные - </span> {bitrixData[1].ENDS }</span>
+                                        <span> <span className="light">последнее воскресенье месяца -   </span>  {bitrixData[1].SAT }    </span>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className="map-block__map">
-
-                                    <div className="map-block__map__contacts">
-                                        <div className="contacts__makhachkala">
-                                            <h3> Город Махачкала </h3>
-                                            <div className="contacts__makhachkala__numbers">
-                                                <p> улица Хурукринская, 11 </p>
-                                                <span className="my-1"> {bitrixData[1].NUM1 }</span>
-                                                <span >{bitrixData[1].NUM2 }</span>
-                                                <span >{bitrixData[1].MAIL }</span>
-                                            </div>
-
-                                            <div className="contacts__makhachkala__numbers">
-                                                <p> Режим работы: </p>
-                                                <span> <span className="light"> будние - </span> {bitrixData[1].DAYS }</span>
-                                                <span>  <span className="light">выходные - </span> {bitrixData[1].ENDS }</span>
-                                                <span> <span className="light">последнее воскресенье месяца -   </span>  {bitrixData[1].SAT }    </span>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="row mt-2 mt-md-4">
+                            <div className="map-block__image col-12 col-md-4 ">
+                                <img className="img-fluid" src={isDesktop ? mosc : mosc2} alt="Москва" />
                             </div>
-
-                            <div className="map-block__map">
-
-                                    <div className="map-block__map__contacts last">
-                                        <div className="contacts__makhachkala">
-                                            <h3> Город Москва </h3>
-                                            <div className="contacts__makhachkala__numbers">
-                                                <p> улица Маршала Новикова, 11 (м. Щукинская)</p>
-                                                <span className="my-1"> {bitrixData[2].NUM1 }</span>
-                                                <span >{bitrixData[2].NUM2 }</span>
-                                                <span >{bitrixData[2].MAIL }</span>
-                                            </div>
-
-                                            <div className="contacts__makhachkala__numbers">
-                                                <p> Режим работы: </p>
-                                                <span> <span className="light"> каждый день - </span> {bitrixData[1].DAYS }</span>
-                                            </div>
+                            <div className="map-block__block col-12 col-md-8 row">
+                                <h2 className="col-12"> Город Москва </h2>
+                                <div className="col-12 col-md-6">
+                                        <p className="light"> улица Маршала Новикова, 11 (м. Щукинская)</p>
+                                        <div>
+                                            <span className="my-1"> {bitrixData[2].NUM1 }</span>
+                                            <span >{bitrixData[2].NUM2 }</span>
+                                            <span >{bitrixData[2].MAIL }</span>
+                                        <div>
+                                            <p> Режим работы: </p>
+                                            <span> <span className="light"> каждый день - </span> {bitrixData[1].DAYS }</span>
                                         </div>
-                                    </div>
+                                </div>
+                                <div className="col-6"></div>
                             </div>
+                        </div>
+                        </div>
 
                     </div>
                 </section>
